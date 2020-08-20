40cfe3e8b7f0980a4fe5966a650b3c5b035ffcfb
Darlan Alves <me@darlanalv.es>
2020-08-20 21:50:32 +0200

    feat: update to v2 syntax

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import { lambda } from '@node-lambdas/core';
 
 const configutation = {
-  version: 1,
+  version: 2,
+  actions: {
+    echo: {
+      default: true,
+      handler: (input, output) => input.pipe(output),
+    }
+  }
 };
 
-lambda(configutation, (input, output) => input.pipe(output));
+lambda(configutation);
